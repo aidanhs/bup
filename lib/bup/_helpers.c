2ac87209760afe14416838c804914ef71b9395a5
Aidan Hobson Sayers <aidanhs@cantab.net>
2013-10-22 15:40:38 +0100

    Simple iterator implementation

--- lib/bup/_helpers.c
+++ lib/bup/_helpers.c
@@ -676,6 +676,90 @@ static PyObject *open_noatime(PyObject *self, PyObject *args)
 }
 
 
+typedef struct {
+    PyObject_HEAD
+    long int m;
+    long int i;
+} spam_MyIter;
+
+PyObject* spam_MyIter_iter(PyObject *self)
+{
+    Py_INCREF(self);
+    return self;
+}
+
+PyObject* spam_MyIter_iternext(PyObject *self)
+{
+    spam_MyIter *p = (spam_MyIter *)self;
+    if (p->i < p->m) {
+        PyObject *tmp = Py_BuildValue("l", p->i);
+        (p->i)++;
+        return tmp;
+    } else {
+        /* Raising of standard StopIteration exception with empty value. */
+        PyErr_SetNone(PyExc_StopIteration);
+        return NULL;
+    }
+}
+
+static PyTypeObject spam_MyIterType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "_helpers._MyIter",        /*tp_name*/
+    sizeof(spam_MyIter),       /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    0,                         /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_ITER,
+      /* tp_flags: Py_TPFLAGS_HAVE_ITER tells python to
+         use tp_iter and tp_iternext fields. */
+    "Internal myiter iterator object.",           /* tp_doc */
+    0,  /* tp_traverse */
+    0,  /* tp_clear */
+    0,  /* tp_richcompare */
+    0,  /* tp_weaklistoffset */
+    spam_MyIter_iter,  /* tp_iter: __iter__() method */
+    spam_MyIter_iternext  /* tp_iternext: next() method */
+};
+
+static PyObject *
+spam_myiter(PyObject *self, PyObject *args)
+{
+    long int m;
+    spam_MyIter *p;
+
+    if (!PyArg_ParseTuple(args, "l", &m))  return NULL;
+
+    /* I don't need python callable __init__() method for this iterator,
+       so I'll simply allocate it as PyObject and initialize it by hand. */
+
+    p = PyObject_New(spam_MyIter, &spam_MyIterType);
+    if (!p) return NULL;
+
+    /* I'm not sure if it's strictly necessary. */
+    if (!PyObject_Init((PyObject *)p, &spam_MyIterType)) {
+        Py_DECREF(p);
+        return NULL;
+    }
+
+    p->m = m;
+    p->i = 0;
+    return (PyObject *)p;
+}
+
 static PyObject *fadvise_done(PyObject *self, PyObject *args)
 {
     int fd = -1;
@@ -1077,6 +1161,8 @@ static PyObject *bup_fstat(PyObject *self, PyObject *args)
 
 
 static PyMethodDef helper_methods[] = {
+    {"myiter",  spam_myiter, METH_VARARGS, "Iterate from i=0 while i<m."},
+
     { "selftest", selftest, METH_VARARGS,
 	"Check that the rolling checksum rolls correctly (for unit tests)." },
     { "blobbits", blobbits, METH_VARARGS,
@@ -1145,9 +1231,17 @@ PyMODINIT_FUNC init_helpers(void)
     assert(sizeof(PY_LONG_LONG) <= sizeof(long long));
 
     char *e;
+
+    spam_MyIterType.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&spam_MyIterType) < 0)  return;
+
     PyObject *m = Py_InitModule("_helpers", helper_methods);
     if (m == NULL)
         return;
+
+    Py_INCREF(&spam_MyIterType);
+    PyModule_AddObject(m, "_MyIter", (PyObject *)&spam_MyIterType);
+
     e = getenv("BUP_FORCE_TTY");
     istty2 = isatty(2) || (atoi(e ? e : "0") & 2);
     unpythonize_argv();
