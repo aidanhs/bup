9d9769e57cd95eb232fb5f24cf04510935213d8c
Aidan Hobson Sayers <aidanhs@cantab.net>
2013-10-28 16:49:07 +0000

    Accept a list of files to iterate over

--- lib/bup/_hashsplit.c
+++ lib/bup/_hashsplit.c
@@ -9,21 +9,27 @@
 
 typedef struct {
     PyObject_HEAD
-    long fd;
+    long *fds;
     long long ofs;
-    PyObject *file;
+    Py_ssize_t curfile;
+    Py_ssize_t numfiles;
+    PyObject *files;
     char buf[1024*1024];
 } readfile_iter_state;
 
 PyObject* readfile_iter_iternext(PyObject *self)
 {
     readfile_iter_state *p = (readfile_iter_state *)self;
-    ssize_t num = read(p->fd, p->buf, 1024*1024);
+    ssize_t num = read(p->fds[p->curfile], p->buf, 1024*1024);
     if (num > 0) {
         p->ofs += num;
         PyObject *tmp = Py_BuildValue("s#", p->buf, num);
         return tmp;
-    } else if (num == 0) {
+    } else if (num == 0 && p->curfile != p->numfiles - 1) { /* End of file */
+        p->ofs = 0;
+        p->curfile = p->curfile + 1;
+        return readfile_iter_iternext(self);
+    } else if (num == 0) { /* End of the file */
         /* Raising of standard StopIteration exception with empty value. */
         PyErr_SetNone(PyExc_StopIteration);
         return NULL;
@@ -36,29 +42,52 @@ PyObject* readfile_iter_iternext(PyObject *self)
 static PyObject *
 readfile_iter_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 {
+    PyObject *files;
+
+    if (!PyArg_ParseTuple(args, "O:fread", &files))
+        return NULL;
+
+    /* We expect an argument that supports the sequence protocol */
+    if (!PySequence_Check(files)) {
+        PyErr_SetString(PyExc_TypeError, "readfile_iter() expects a sequence");
+        return NULL;
+    }
+
+    Py_ssize_t len = PySequence_Length(files);
+    if (len == -1)
+        return NULL;
+
+    long *fds = calloc(sizeof(long), len);
+    Py_ssize_t i;
+    PyObject *fileno;
     PyObject *file;
-    long fd = -1;
+    for (i = 0; i < len; i++) {
+        file = PySequence_GetItem(files, i);
+        if (file == NULL)
+            return NULL;
+        fileno = PyObject_CallMethod(file, "fileno", NULL);
+        Py_DECREF(file);
+        if (fileno == NULL)
+            return NULL;
+        fds[i] = PyInt_AsLong(fileno);
+        Py_DECREF(fileno);
+        /* Assume error on -1 because this is a file descriptor */
+        if (fds[i] == -1)
+            return NULL;
+    }
 
-    if (!PyArg_ParseTuple(args, "O:fread", &file))
-        return NULL;
-
-    PyObject *fileno = PyObject_CallMethod(file, "fileno", NULL);
-    if (fileno == NULL)
-        return NULL;
-
-    fd = PyInt_AsLong(fileno);
-    Py_DECREF(fileno);
-    if (fd == -1)
-        return NULL;
-
-    readfile_iter_state *freadstate = (readfile_iter_state *)type->tp_alloc(type, 0);
+    readfile_iter_state *freadstate =
+        (readfile_iter_state *)type->tp_alloc(type, 0);
     if (!freadstate)
         return NULL;
 
-    Py_INCREF(file);
-    freadstate->file = file;
-    freadstate->fd = fd;
+    /* Make sure python doesn't garbage collect and close the files */
+    Py_INCREF(files);
+    freadstate->files = files;
+    freadstate->fds = fds;
     freadstate->ofs = 0;
+    freadstate->curfile = 0;
+    freadstate->numfiles = len;
 
     return (PyObject *)freadstate;
 }
@@ -66,7 +95,9 @@ readfile_iter_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
 static void
 fread_dealloc(PyObject *freadstate)
 {
-    Py_DECREF(((readfile_iter_state *)freadstate)->file);
+    readfile_iter_state *state = (readfile_iter_state *)freadstate;
+    free(state->fds);
+    Py_DECREF(state->files);
     Py_TYPE(freadstate)->tp_free(freadstate);
 }
 
